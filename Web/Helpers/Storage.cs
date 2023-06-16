581c4047b803110fc7e5cd378094c35dd447d417
Nicolas <nicomorantjuan@gmail.com>
2023-06-16 12:48:03 +0200

    ..

--- Web/Helpers/Storage.cs
+++ Web/Helpers/Storage.cs
@@ -1,4 +1,5 @@
 ﻿using API.Helpers;
+using static API.Controllers.InformesControllers.InformesVehiculoController;
 
 namespace Web.Helpers
 {
@@ -7,7 +8,7 @@ namespace Web.Helpers
     {
         public static string? token { get; set; }
         public static Role? role { get; set; }
-        public static HttpClient httpclient { get; set;}
-
+        public static ConsumoVehiculoId consumoVehiculoId { get; set; }
+        public static List<ConsumoMes> consumoMes { get; set; }
     }
 }
